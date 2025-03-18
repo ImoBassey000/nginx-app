319009bef5889f5849a3f50903eda6aacb84b288
ImoBassey000 <ibassey000@gmail.com>
2025-03-18 10:12:54 +0000

    update

--- terraform/modules/key-pair/main.tf
+++ terraform/modules/key-pair/main.tf
@@ -1,11 +1,11 @@
-resource "tls_private_key" "key_pair" {
+resource "tls_private_key" "private_key_pair" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
 resource "aws_key_pair" "my-key-pair" {
   key_name   = var.key_name
-  public_key = tls_private_key.key_pair.public_key_openssh
+  public_key = tls_private_key.private_key_pair.public_key_openssh
 }
 
 output "key_name" {
